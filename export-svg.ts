708243a1fa965b87756ce4b941d1898a43975a6a
3commascapital <3commascapital+pulsechain@gmail.com>
2025-03-19 17:49:13 -0500

    build: update @types/node to version 22.13.10
    - Upgrade @types/node from 20.14.8 to 22.13.10
    - Update related dependencies in pnpm-lock.yaml
    - Enable downlevelIteration in tsconfig.json
    
    BREAKING CHANGE: Major version update of @types/node may introduce breaking changes

--- export-svg.ts
+++ export-svg.ts
@@ -1,40 +1,44 @@
 import { readFile } from 'node:fs/promises'
 import { IconSet } from '@iconify/tools'
-import path from 'node:path'
+import * as path from 'path'
 import { blankIconSet } from '@iconify/tools'
 import { allIconKeys } from './src/renderer/src/lib/components/icon'
-import * as fs from 'node:fs'
+import * as fs from 'fs'
 
 // Directories
-const cacheDir = ['node_modules', '@iconify', 'json']
-const outDir = ['src', 'renderer', 'src', 'lib', 'svg']
-// Get a list of icon sets
-const list = JSON.parse(await readFile(path.join(...cacheDir, 'collections.json'), 'utf8'))
-const prefixes = Object.keys(list)
+const main = async () => {
+  const cacheDir = ['node_modules', '@iconify', 'json']
+  const outDir = ['src', 'renderer', 'src', 'lib', 'svg']
+  // Get a list of icon sets
+  const list = JSON.parse(await readFile(path.join(...cacheDir, 'collections.json'), 'utf8'))
+  const prefixes = Object.keys(list)
 
-// Export each icon set
-for (let i = 0; i < prefixes.length; i++) {
-  const prefix = prefixes[i]
-  const set = allIconKeys.get(prefix)
-  if (!set) {
-    continue
-  }
-  // Read file
-  const data = JSON.parse(await readFile(path.join(...cacheDir, 'json', prefix + '.json'), 'utf8'))
-
-  const emptySet = blankIconSet(prefix)
-
-  // Create IconSet
-  const iconSet = new IconSet(data)
-  for (const key of set.values()) {
-    if (!iconSet.exists(key)) {
-      console.log(`icon ${prefix}:${key} is missing!`)
+  // Export each icon set
+  for (let i = 0; i < prefixes.length; i++) {
+    const prefix = prefixes[i]
+    const set = allIconKeys.get(prefix)
+    if (!set) {
+      continue
     }
-    const icon = iconSet.resolve(key)!
-    emptySet.setIcon(key, icon)
+    // Read file
+    const data = JSON.parse(await readFile(path.join(...cacheDir, 'json', prefix + '.json'), 'utf8'))
+
+    const emptySet = blankIconSet(prefix)
+
+    // Create IconSet
+    const iconSet = new IconSet(data)
+    for (const key of Array.from(set.values())) {
+      if (!iconSet.exists(key)) {
+        console.log(`icon ${prefix}:${key} is missing!`)
+      }
+      const icon = iconSet.resolve(key)!
+      emptySet.setIcon(key, icon)
+    }
+    const json = emptySet.export(true)
+    const jsonPath = path.join(...outDir, `${prefix}.json`)
+    await fs.promises.mkdir(path.dirname(jsonPath), { recursive: true })
+    await fs.promises.writeFile(jsonPath, JSON.stringify(json, null, 2))
   }
-  const json = emptySet.export(true)
-  const jsonPath = path.join(...outDir, `${prefix}.json`)
-  await fs.promises.mkdir(path.dirname(jsonPath), { recursive: true })
-  await fs.promises.writeFile(jsonPath, JSON.stringify(json, null, 2))
 }
+
+main()
